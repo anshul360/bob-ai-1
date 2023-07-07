57e2d52a7a4fab372e98ba56b344d2b2badf50d0
Anshul Kumar <got.trapped.in.the.matrix@gmail.com>
2023-07-07 22:52:36 +0530

    trying to resolve 50mb issue on vercel

--- library/documents/langChain/webLoader.ts
+++ library/documents/langChain/webLoader.ts
@@ -1,56 +1,55 @@
-import { Browser, Page, PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
+// import { Browser, Page, PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
 import chromium from 'chrome-aws-lambda';
 import { Document } from "langchain/document";
 
 export default async function webLoader (path: string) {
-    const loader = new PuppeteerWebBaseLoader("https://scouthangout.com", {
-        launchOptions: {
-            headless: "new",
-            executablePath: await chromium.executablePath
-        },
-        gotoOptions: {
-            waitUntil: "domcontentloaded",
-            timeout: 60000,
-        },
+    // const loader = new PuppeteerWebBaseLoader("https://scouthangout.com", {
+    //     launchOptions: {
+    //         headless: "new",
+    //         executablePath: await chromium.executablePath
+    //     },
+    //     gotoOptions: {
+    //         waitUntil: "domcontentloaded",
+    //         timeout: 60000,
+    //     },
         
-        /** Pass custom evaluate, in this case you get page and browser instances */
-        async evaluate(page: Page, browser: Browser) {
-            // let browser1 = await chromium.puppeteer.launch({
-            //     args: chromium.args,
-            //     defaultViewport: chromium.defaultViewport,
-            //     executablePath: await chromium.executablePath,
-            //     headless: true,
-            //     ignoreHTTPSErrors: true,
-            // });
+    //     /** Pass custom evaluate, in this case you get page and browser instances */
+    //     async evaluate(page: Page, browser: Browser) {
+    //         // let browser1 = await chromium.puppeteer.launch({
+    //         //     args: chromium.args,
+    //         //     defaultViewport: chromium.defaultViewport,
+    //         //     executablePath: await chromium.executablePath,
+    //         //     headless: true,
+    //         //     ignoreHTTPSErrors: true,
+    //         // });
         
-            // let page1 = await browser1.newPage();
-            // console.log("-=-=-=-=async started");
-            await page.goto("https://scouthangout.com", {timeout: 60000, waitUntil: "domcontentloaded"});
-            // console.log("-=-=-=-=page loaded");
+    //         // let page1 = await browser1.newPage();
+    //         // console.log("-=-=-=-=async started");
+    //         await page.goto("https://scouthangout.com", {timeout: 60000, waitUntil: "domcontentloaded"});
+    //         // console.log("-=-=-=-=page loaded");
 
-            const result = await page.$eval('*', (el: any) => el.innerText)//await page.evaluate(() => document.body.innerHTML);
-            // console.log("-=-=-=-=got result-=-=", result);
-            return result;
-        },
-    });
-
-    // let browser1 = await chromium.puppeteer.launch({
-    //     args: chromium.args,
-    //     defaultViewport: chromium.defaultViewport,
-    //     executablePath: await chromium.executablePath,
-    //     headless: true,
-    //     ignoreHTTPSErrors: true,
+    //         const result = await page.$eval('*', (el: any) => el.innerText)//await page.evaluate(() => document.body.innerHTML);
+    //         // console.log("-=-=-=-=got result-=-=", result);
+    //         return result;
+    //     },
     // });
 
-    // let page1 = await browser1.newPage();
-    // // console.log("-=-=-=-=async started");
-    // await page1.goto("https://scouthangout.com", {timeout: 60000, waitUntil: "domcontentloaded"});
-    // // console.log("-=-=-=-=page loaded");
+    let browser1 = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+    });
 
-    // const result = await page1.$eval('*', (el: any) => el.innerText)
+    let page1 = await browser1.newPage();
+    
+    await page1.goto("https://scouthangout.com", {timeout: 60000, waitUntil: "domcontentloaded"});
 
-    const docs = await loader.load();
-    // const docs = [new Document({pageContent: result, metadata: {source: path}})];
+    const result = await page1.$eval('*', (el: any) => el.innerText)
+
+    // const docs = await loader.load();
+    const docs = [new Document({pageContent: result, metadata: {source: path}})];
 
     let charCount = 0;
     docs.map((doc => charCount+=doc.pageContent.length))
