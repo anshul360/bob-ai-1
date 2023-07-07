18f18329d9e664cbf9de208fcff42f96eedc034c
Anshul Kumar <got.trapped.in.the.matrix@gmail.com>
2023-07-07 22:10:43 +0530

    puppeteer issue fix: chrome-aws-lambda headless chrome

--- library/documents/langChain/webLoader.ts
+++ library/documents/langChain/webLoader.ts
@@ -1,16 +1,29 @@
 import { Browser, Page, PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
+import chromium from 'chrome-aws-lambda';
+import { Document } from "langchain/document";
 
 export default async function webLoader (path: string) {
     const loader = new PuppeteerWebBaseLoader("https://scouthangout.com", {
         launchOptions: {
             headless: "new",
+            executablePath: await chromium.executablePath
         },
         gotoOptions: {
             waitUntil: "domcontentloaded",
             timeout: 60000,
         },
+        
         /** Pass custom evaluate, in this case you get page and browser instances */
         async evaluate(page: Page, browser: Browser) {
+            // let browser1 = await chromium.puppeteer.launch({
+            //     args: chromium.args,
+            //     defaultViewport: chromium.defaultViewport,
+            //     executablePath: await chromium.executablePath,
+            //     headless: true,
+            //     ignoreHTTPSErrors: true,
+            // });
+        
+            // let page1 = await browser1.newPage();
             // console.log("-=-=-=-=async started");
             await page.goto("https://scouthangout.com", {timeout: 60000, waitUntil: "domcontentloaded"});
             // console.log("-=-=-=-=page loaded");
@@ -21,7 +34,23 @@ export default async function webLoader (path: string) {
         },
     });
 
+    // let browser1 = await chromium.puppeteer.launch({
+    //     args: chromium.args,
+    //     defaultViewport: chromium.defaultViewport,
+    //     executablePath: await chromium.executablePath,
+    //     headless: true,
+    //     ignoreHTTPSErrors: true,
+    // });
+
+    // let page1 = await browser1.newPage();
+    // // console.log("-=-=-=-=async started");
+    // await page1.goto("https://scouthangout.com", {timeout: 60000, waitUntil: "domcontentloaded"});
+    // // console.log("-=-=-=-=page loaded");
+
+    // const result = await page1.$eval('*', (el: any) => el.innerText)
+
     const docs = await loader.load();
+    // const docs = [new Document({pageContent: result, metadata: {source: path}})];
 
     let charCount = 0;
     docs.map((doc => charCount+=doc.pageContent.length))
--- next.config.js
+++ next.config.js
@@ -3,8 +3,21 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
-    serverActions: true
-  }
+    serverActions: true,
+    serverComponentsExternalPackages: ['chrome-aws-lambda']
+  },
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  //   //config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+
+  //   // This exists to keep the package size below the lambda 50mb zipped limit
+  //   if (isServer) {
+  //     if (!dev) {
+  //       config.externals = ['chrome-aws-lambda'];
+  //     }
+  //   }
+
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
