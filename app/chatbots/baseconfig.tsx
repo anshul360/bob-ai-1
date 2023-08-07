453e327638f5eb7925ad549c258fc6a42506b006
Anshul Kumar <got.trapped.in.the.matrix@gmail.com>
2023-08-07 17:48:55 +0530

    slight cosmetic change to baseconfig

--- app/chatbots/baseconfig.tsx
+++ app/chatbots/baseconfig.tsx
@@ -83,15 +83,15 @@ export default function Baseconfig({botId, user}: any) {
     const switchclass = `mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 
     before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full 
     after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] 
-    after:content-[''] checked:bg-cyan-500 checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 
-    checked:after:rounded-full checked:after:border-none checked:after:bg-cyan-500 
+    after:content-[''] checked:bg-[#00FFFF] checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 
+    checked:after:rounded-full checked:after:border-none checked:after:bg-[#00FFFF] 
     checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] 
     checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:before:scale-100 
     focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] 
     focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] 
-    checked:focus:border-primary checked:focus:bg-cyan-500 checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 
+    checked:focus:border-primary checked:focus:bg-[#00FFFF] checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 
     checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 
-    dark:checked:bg-cyan-500 dark:checked:after:bg-cyan-500`;
+    dark:checked:bg-[#00FFFF] dark:checked:after:bg-[#00FFFF]`;
     
     return<>
         <div className=" flex w-full gap-4 flex-col md:flex-row ">
