add0b66aca9da04943d1b32c424f95c3e8445c58
Anshul Kumar <got.trapped.in.the.matrix@gmail.com>
2023-12-20 17:20:14 +0530

    CORS issue resolved

--- middleware.ts
+++ middleware.ts
@@ -16,6 +16,15 @@ export async function middleware(req: NextRequest) {
 
     if (req.nextUrl.pathname.startsWith('/api/v1')) {
         console.log("inside api v1");
+        const origin = req.headers.get("origin");
+        res.headers.append('Access-Control-Allow-Origin', origin!);
+        res.headers.append('Access-Control-Allow-Credentials', "true")
+        res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+        res.headers.append(
+            'Access-Control-Allow-Headers',
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
+
         const btoken = req.headers.get("Authorization");
         if (btoken) {
             try {
