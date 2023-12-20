2b1f0647fdc0303d030a9be4f98f0552edce40bf
Anshul Kumar <got.trapped.in.the.matrix@gmail.com>
2023-12-20 20:11:48 +0530

    trying middleware fix

--- middleware.ts
+++ middleware.ts
@@ -24,6 +24,16 @@ export async function middleware(req: NextRequest) {
         //     'Access-Control-Allow-Headers',
         //     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
         // )
+        const res_headers = {
+            'Access-Control-Allow-Origin': origin!,
+            'Access-Control-Allow-Credentials': "true",
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version',
+            "Access-Control-Max-Age": "86400"
+        }
+        if (req.method === 'OPTIONS') {
+            return NextResponse.json({}, {headers: res_headers})
+        }
 
         const btoken = req.headers.get("Authorization");
         if (btoken) {
